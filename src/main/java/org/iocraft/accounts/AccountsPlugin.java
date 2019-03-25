95c04e0ecf62d0969168901b2d239f9d455c3680
io-craft <io_craft@protonmail.com>
2019-03-25 06:21:30 -0500

    Automation, bleh!

--- src/main/java/org/iocraft/accounts/AccountsPlugin.java
+++ src/main/java/org/iocraft/accounts/AccountsPlugin.java
@@ -3,15 +3,15 @@ package org.iocraft.accounts;
 import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BunnyStormPlugin extends JavaPlugin {
+public class AccountsPlugin extends JavaPlugin {
     
-  static final String pluginShortName = "BunnyStorm";
+  static final String pluginShortName = "IO-Accounts";
   
   @Override
   public void onEnable() {
     //getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
 
-    this.getCommand("bunnystorm").setExecutor(new CommandBunnyStorm());
+    this.getCommand("accounts").setExecutor(new CommandAccounts());
     this.getCommand("rabbatoir").setExecutor(new CommandRabbatoir());
   }
 
@@ -19,7 +19,7 @@ public class BunnyStormPlugin extends JavaPlugin {
   public void onDisable() {
     //getLogger().log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
 
-    this.getCommand("bunnystorm").setExecutor(null); // not sure what the right
+    this.getCommand("accounts").setExecutor(null); // not sure what the right
     this.getCommand("rabbatoir").setExecutor(null);  // action here is.
   }
 }
